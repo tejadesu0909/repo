8b9f8611915593029760d934097840a7d4028c40
tejadesu0909 <desu.teja5@gmail.com>
2021-10-23 22:50:58 +0530

    c2

--- src/com/practise1/p1.java
+++ src/com/practise1/p1.java
@@ -4,7 +4,7 @@ public class p1 {
 	
 	public static void main(String[] args) {
 		System.out.println("2nd package 1st class");
-		
+		System.out.println("merge conflict");
 	}
 
 }
---
+++ src/com/practise1/p2.java
@@ -0,0 +1,5 @@
+package com.practise1;
+
+public class p2 {
+
+}
