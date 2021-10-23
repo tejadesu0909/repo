249004b480134afa78a48e4e25d84b84e4e71e59
tejadesu0909 <desu.teja5@gmail.com>
2021-10-23 22:26:57 +0530

    1

--- src/com/practise/p1.java
+++ src/com/practise/p1.java
@@ -4,11 +4,6 @@ public class p1 {
 
 	public static void main(String[] args) {
 		System.out.println("Running the first sop");
-<<<<<<< HEAD
-		System.out.println("istmerge conflict");
-	}
 
-=======
-	System.out.println("merge conflict");}
->>>>>>> 8be56fc5afa13003407832491b65a1d501a068ab
-}
+	}
+	}
