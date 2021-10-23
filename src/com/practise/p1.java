9e65695fe46f685159e5e5749469e382b6391be5
tejadesu0909 <desu.teja5@gmail.com>
2021-10-23 15:54:07 +0530

    package2

--- src/com/practise/p1.java
+++ src/com/practise/p1.java
@@ -1,7 +1,7 @@
 package com.practise;
 
 public class p1 {
-	
+
 	public static void main(String[] args) {
 		System.out.println("Running the first sop");
 		System.out.println("p2");
---
+++ src/com/practise1/p1.java
@@ -0,0 +1,10 @@
+package com.practise1;
+
+public class p1 {
+	
+	public static void main(String[] args) {
+		System.out.println("2nd package 1st class");
+		
+	}
+
+}
